2e7730923a46d3304b762f9e33fb498978e89662
ckrausko <ckrausko@gmail.com>
2017-02-15 19:00:23 -0500

    complex data model

--- Models/Enrollment.cs
+++ Models/Enrollment.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public enum Grade
     {
-        A,B,C,D,F
+        A, B, C, D, F
     }
 
     public class Enrollment
@@ -15,6 +13,7 @@ namespace ContosoUniversity.Models
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
